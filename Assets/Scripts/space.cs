1af310237280f6891b42a3543b8ade7ccc92559d
Noelle <noelledino@gmail.com>
2017-09-26 14:40:03 -0400

    commit

---
+++ Assets/Scripts/space.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class space : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetKey( KeyCode.Space ) ) {
+			rb.isKinematic = false;
+	}
+}
+}
---
+++ Assets/Scripts/winTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class winTrigger : MonoBehaviour {
+
+	public Text win;
+	void OnTriggerEnter() {
+		win.text = "You win!";
+	}
+}
